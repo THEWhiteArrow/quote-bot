97cdb6e596ec0007c8250dc9e539467fe421e56e
Damian Trafialek <damian.trafialk@gmail.com>
2023-08-14 09:58:11 +0200

    add: clean up main

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import shutil
-from moviepy.editor import *
-from gtts import gTTS as gtts
-from TTS.GTTS import GTTS
+from moviepy.editor import * 
 from quotes.quotes_wrapper import get_quotes
 from utils.toml_helper import *
 from video_creation.background import *
 from utils.ffmpeg_install import *
 from TTS.TikTok import *
 from video_creation.assemble import *
-import pyttsx3
 
-def convert_video():
-    clip = VideoFileClip("./goku.mp4")
-
-    text_q = TextClip("Hello World", fontsize=20, color="white", size=clip.size)
-    text_q = text_q.set_duration(5).set_pos("center").set_opacity(0.6)
-
-    final_clip = CompositeVideoClip([clip, text_q])
-    final_clip.write_videofile("final.mp4", fps=24)
-
-
-def convert_tts():
-    tts = gtts("<speak>Hello world<mark/> yo how u doing?</speak>", lang="en")
-    tts.save("hello.mp3")
 
 
 def prepare_toml():
